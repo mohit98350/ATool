9488ec5eb621c0d4c31e1015c4b2a6f6aecb582f
mohit <mohitmayank26@gmail.com>
2022-06-16 21:57:50 +0530

    added compaign init

--- src/components/Home/HomeView.js
+++ src/components/Home/HomeView.js
@@ -2,27 +2,30 @@ import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
+import List from '../LIst/List';
 const HomeView = () => {
   return (
     <div>
-        <Navbar/>
-        <div className='image'>
-        <img src = "https://groundwork.azavea.com/app/groundwork_geospatial_context_illo.svg"/>
-        <p style={{marginLeft:'30px'}}>Start a labeling campaign</p>
+      <Navbar />
+      <div className='image'>
+        <img src="https://groundwork.azavea.com/app/groundwork_geospatial_context_illo.svg" />
+        <p style={{ marginLeft: '30px' }}>Start a labeling campaign</p>
         < Button style={{
-                        borderRadius: 20,
-                        width:'210px',
-                        fontSize: "15px",
-                        marginLeft:'1rem',
-                        marginTop:'1rem'
-                        
-                    }}
-                    
-                    variant="contained" startIcon={<AddIcon/>} >
-                     new campaigns
-                    </Button>
-        </div>
-      
+          borderRadius: 20,
+          width: '210px',
+          fontSize: "15px",
+          marginLeft: '1rem',
+          marginTop: '1rem'
+
+        }}
+
+          variant="contained" startIcon={<AddIcon />} >
+          new campaigns
+        </Button>
+        <List />
+
+      </div>
+
     </div>
   )
 }
